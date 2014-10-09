b9735f389939bf8200f06c717968a250e74ea2b4
ivanchoo <ivanchoo.private@gmail.com>
2014-10-09 17:59:04 +0800

    Initial Commit

---
+++ Constraint/Constraint.h
@@ -0,0 +1,19 @@
+//
+//  Constraint.h
+//  Constraint
+//
+//  Created by Ivan Choo on 9/10/14.
+//  Copyright (c) 2014 ADD Creativeworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Constraint.
+FOUNDATION_EXPORT double ConstraintVersionNumber;
+
+//! Project version string for Constraint.
+FOUNDATION_EXPORT const unsigned char ConstraintVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Constraint/PublicHeader.h>
+
+
